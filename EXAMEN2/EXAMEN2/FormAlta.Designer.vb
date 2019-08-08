fc9847f52c6e9b3c17c0ac108b3e72c90549d3cc
priverocervantes <finale87@gmail.com>
2019-08-08 18:19:37 -0300

    nuevas asignaciones de variables

--- EXAMEN2/EXAMEN2/FormAlta.Designer.vb
+++ EXAMEN2/EXAMEN2/FormAlta.Designer.vb
@@ -29,16 +29,25 @@ Partial Class FormAlta
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtImporteCuotaAP = New System.Windows.Forms.TextBox()
         Me.txtFechaContratoAP = New System.Windows.Forms.TextBox()
         Me.btnIrGrilla = New System.Windows.Forms.Button()
-        Me.cbxPlanAP = New System.Windows.Forms.ComboBox()
+        Me.cbxCantCuotasPlanAP = New System.Windows.Forms.ComboBox()
         Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtImporteCtaAP = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtDescripcionAP = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtPrecioAP = New System.Windows.Forms.TextBox()
+        Me.cbxMarcaAP = New System.Windows.Forms.ComboBox()
+        Me.cbxModeloAP = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
         'btnAltaPlan
         '
-        Me.btnAltaPlan.Location = New System.Drawing.Point(190, 315)
+        Me.btnAltaPlan.Location = New System.Drawing.Point(182, 371)
         Me.btnAltaPlan.Name = "btnAltaPlan"
         Me.btnAltaPlan.Size = New System.Drawing.Size(151, 46)
         Me.btnAltaPlan.TabIndex = 21
@@ -48,7 +57,7 @@ Partial Class FormAlta
         'cbxVendedorAP
         '
         Me.cbxVendedorAP.FormattingEnabled = True
-        Me.cbxVendedorAP.Location = New System.Drawing.Point(241, 169)
+        Me.cbxVendedorAP.Location = New System.Drawing.Point(231, 60)
         Me.cbxVendedorAP.Name = "cbxVendedorAP"
         Me.cbxVendedorAP.Size = New System.Drawing.Size(154, 21)
         Me.cbxVendedorAP.TabIndex = 20
@@ -56,7 +65,7 @@ Partial Class FormAlta
         'cbxClienteAP
         '
         Me.cbxClienteAP.FormattingEnabled = True
-        Me.cbxClienteAP.Location = New System.Drawing.Point(241, 207)
+        Me.cbxClienteAP.Location = New System.Drawing.Point(231, 98)
         Me.cbxClienteAP.Name = "cbxClienteAP"
         Me.cbxClienteAP.Size = New System.Drawing.Size(154, 21)
         Me.cbxClienteAP.TabIndex = 19
@@ -64,16 +73,16 @@ Partial Class FormAlta
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(124, 277)
+        Me.Label4.Location = New System.Drawing.Point(116, 174)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(96, 13)
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
         Me.Label4.TabIndex = 18
-        Me.Label4.Text = "IMPORTE CUOTA"
+        Me.Label4.Text = "MODELO"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(124, 211)
+        Me.Label3.Location = New System.Drawing.Point(113, 102)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(52, 13)
         Me.Label3.TabIndex = 17
@@ -82,7 +91,7 @@ Partial Class FormAlta
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(124, 173)
+        Me.Label2.Location = New System.Drawing.Point(113, 64)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(68, 13)
         Me.Label2.TabIndex = 16
@@ -91,58 +100,143 @@ Partial Class FormAlta
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(124, 134)
+        Me.Label1.Location = New System.Drawing.Point(113, 25)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(105, 13)
         Me.Label1.TabIndex = 15
         Me.Label1.Text = "FECHA CONTRATO"
         '
-        'txtImporteCuotaAP
-        '
-        Me.txtImporteCuotaAP.Location = New System.Drawing.Point(241, 274)
-        Me.txtImporteCuotaAP.Name = "txtImporteCuotaAP"
-        Me.txtImporteCuotaAP.Size = New System.Drawing.Size(100, 20)
-        Me.txtImporteCuotaAP.TabIndex = 14
-        '
         'txtFechaContratoAP
         '
-        Me.txtFechaContratoAP.Location = New System.Drawing.Point(241, 134)
+        Me.txtFechaContratoAP.Location = New System.Drawing.Point(231, 25)
         Me.txtFechaContratoAP.Name = "txtFechaContratoAP"
         Me.txtFechaContratoAP.Size = New System.Drawing.Size(100, 20)
         Me.txtFechaContratoAP.TabIndex = 13
         '
         'btnIrGrilla
         '
-        Me.btnIrGrilla.Location = New System.Drawing.Point(190, 389)
+        Me.btnIrGrilla.Location = New System.Drawing.Point(182, 445)
         Me.btnIrGrilla.Name = "btnIrGrilla"
         Me.btnIrGrilla.Size = New System.Drawing.Size(151, 46)
         Me.btnIrGrilla.TabIndex = 22
         Me.btnIrGrilla.Text = "VER GRILLA"
         Me.btnIrGrilla.UseVisualStyleBackColor = True
         '
-        'cbxPlanAP
+        'cbxCantCuotasPlanAP
         '
-        Me.cbxPlanAP.FormattingEnabled = True
-        Me.cbxPlanAP.Location = New System.Drawing.Point(241, 240)
-        Me.cbxPlanAP.Name = "cbxPlanAP"
-        Me.cbxPlanAP.Size = New System.Drawing.Size(154, 21)
-        Me.cbxPlanAP.TabIndex = 24
+        Me.cbxCantCuotasPlanAP.FormattingEnabled = True
+        Me.cbxCantCuotasPlanAP.Location = New System.Drawing.Point(233, 231)
+        Me.cbxCantCuotasPlanAP.Name = "cbxCantCuotasPlanAP"
+        Me.cbxCantCuotasPlanAP.Size = New System.Drawing.Size(35, 21)
+        Me.cbxCantCuotasPlanAP.TabIndex = 24
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(124, 244)
+        Me.Label5.Location = New System.Drawing.Point(116, 235)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(35, 13)
+        Me.Label5.Size = New System.Drawing.Size(86, 13)
         Me.Label5.TabIndex = 23
-        Me.Label5.Text = "PLAN"
+        Me.Label5.Text = "CANT. CUOTAS"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(116, 269)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(56, 13)
+        Me.Label6.TabIndex = 25
+        Me.Label6.Text = "IMPORTE"
+        '
+        'txtImporteCtaAP
+        '
+        Me.txtImporteCtaAP.Location = New System.Drawing.Point(233, 264)
+        Me.txtImporteCtaAP.Name = "txtImporteCtaAP"
+        Me.txtImporteCtaAP.Size = New System.Drawing.Size(74, 20)
+        Me.txtImporteCtaAP.TabIndex = 26
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(220, 268)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(13, 13)
+        Me.Label7.TabIndex = 27
+        Me.Label7.Text = "$"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(116, 304)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(80, 13)
+        Me.Label8.TabIndex = 29
+        Me.Label8.Text = "DESCRIPCION"
+        '
+        'txtDescripcionAP
+        '
+        Me.txtDescripcionAP.Location = New System.Drawing.Point(233, 301)
+        Me.txtDescripcionAP.Name = "txtDescripcionAP"
+        Me.txtDescripcionAP.Size = New System.Drawing.Size(154, 20)
+        Me.txtDescripcionAP.TabIndex = 28
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(113, 138)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(45, 13)
+        Me.Label9.TabIndex = 31
+        Me.Label9.Text = "MARCA"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(116, 204)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(47, 13)
+        Me.Label10.TabIndex = 33
+        Me.Label10.Text = "PRECIO"
+        '
+        'txtPrecioAP
+        '
+        Me.txtPrecioAP.Location = New System.Drawing.Point(233, 200)
+        Me.txtPrecioAP.Name = "txtPrecioAP"
+        Me.txtPrecioAP.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecioAP.TabIndex = 32
+        '
+        'cbxMarcaAP
+        '
+        Me.cbxMarcaAP.FormattingEnabled = True
+        Me.cbxMarcaAP.Location = New System.Drawing.Point(233, 135)
+        Me.cbxMarcaAP.Name = "cbxMarcaAP"
+        Me.cbxMarcaAP.Size = New System.Drawing.Size(154, 21)
+        Me.cbxMarcaAP.TabIndex = 35
+        '
+        'cbxModeloAP
+        '
+        Me.cbxModeloAP.FormattingEnabled = True
+        Me.cbxModeloAP.Location = New System.Drawing.Point(233, 173)
+        Me.cbxModeloAP.Name = "cbxModeloAP"
+        Me.cbxModeloAP.Size = New System.Drawing.Size(154, 21)
+        Me.cbxModeloAP.TabIndex = 34
         '
         'FormAlta
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(487, 487)
-        Me.Controls.Add(Me.cbxPlanAP)
+        Me.ClientSize = New System.Drawing.Size(487, 539)
+        Me.Controls.Add(Me.cbxMarcaAP)
+        Me.Controls.Add(Me.cbxModeloAP)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.txtPrecioAP)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtDescripcionAP)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtImporteCtaAP)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.cbxCantCuotasPlanAP)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.btnIrGrilla)
         Me.Controls.Add(Me.btnAltaPlan)
@@ -152,7 +246,6 @@ Partial Class FormAlta
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.txtImporteCuotaAP)
         Me.Controls.Add(Me.txtFechaContratoAP)
         Me.Name = "FormAlta"
         Me.Text = "Alta Plan"
@@ -168,9 +261,18 @@ Partial Class FormAlta
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents txtImporteCuotaAP As TextBox
     Friend WithEvents txtFechaContratoAP As TextBox
     Friend WithEvents btnIrGrilla As Button
-    Friend WithEvents cbxPlanAP As ComboBox
+    Friend WithEvents cbxCantCuotasPlanAP As ComboBox
     Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtImporteCtaAP As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtDescripcionAP As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtPrecioAP As TextBox
+    Friend WithEvents cbxMarcaAP As ComboBox
+    Friend WithEvents cbxModeloAP As ComboBox
 End Class
--- EXAMEN2/EXAMEN2/FormAlta.vb
+++ EXAMEN2/EXAMEN2/FormAlta.vb
@@ -7,26 +7,8 @@ Public Class FormAlta
     Dim conn As New ClaseConexion
     Dim Cmd As New SqlCommand
     Dim Da As New SqlDataAdapter
-
-    Private Sub cbxPlanAP_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxPlanAP.SelectedIndexChanged
-        Dim cod As String = cbxPlanAP.SelectedValue.ToString 'aca voy a guardar los valores del valueMember del combo Pais (VAL para convertirlo a numero previo al string)
-        'Dim cod As Integer = Val(cbxPaisP.SelectedValue.ToString) 'aca voy a guardar los valores del valueMember del combo Pais (VAL para convertirlo a numero previo al string)
-        With Cmd
-            .CommandType = CommandType.Text
-            '.CommandText = "Select distinct CiudadDistrito From ciudad where PaisCodigo like '" & cod & "'"
-            .CommandText = "Select RTRIM(CANTIDAD_CUOTAS)+' Ctas - '+RTRIM(DESCRIPCION_CUOTAS) AS PLANES,ID From TIPO_PLAN  where id like '" & cod & "'"
-            .Connection = conn.AbrirConexion
-        End With
-        Da.SelectCommand = Cmd
-        Dt3 = New DataTable 'aca recogere todos los valores
-        Da.Fill(Dt3) 'Relleno dentro de la tabla creada anteriormente
-        With cbxPlanAP 'Recorro y lleno el cbxPaisP con los nombres de la tabla paises
-            .DataSource = Dt3
-            .DisplayMember = "PLANES" 'establece la propiedad que se va a mostrar en el control
-            .ValueMember = "ID"
-        End With
-        '  Cn.Close()
-    End Sub
+    Dim Ds As New DataSet
+    Dim drd As SqlDataReader
 
     Private Sub cbxClienteAP_SelectedValueChanged(sender As Object, e As EventArgs)
         Dim cod As String = cbxClienteAP.SelectedValue.ToString 'aca voy a guardar los valores del valueMember del combo Pais (VAL para convertirlo a numero previo al string)
@@ -40,7 +22,7 @@ Public Class FormAlta
         Da.SelectCommand = Cmd
         Dt3 = New DataTable 'aca recogere todos los valores
         Da.Fill(Dt3) 'Relleno dentro de la tabla creada anteriormente
-        With cbxPlanAP 'Recorro y lleno el cbxPaisP con los nombres de la tabla paises
+        With cbxCantCuotasPlanAP 'Recorro y lleno el cbxPaisP con los nombres de la tabla paises
             .DataSource = Dt3
             .DisplayMember = "PLANES" 'establece la propiedad que se va a mostrar en el control
             .ValueMember = "ID"
@@ -54,10 +36,36 @@ Public Class FormAlta
         '   Dim fecha_actual As Date
     End Sub
 
+    Private Sub cbxMarcaAP_SelectedValueChanged(sender As Object, e As EventArgs) Handles cbxMarcaAP.SelectedValueChanged
+        Dim cod As String = cbxMarcaAP.SelectedValue.ToString 'aca voy a guardar los valores del valueMember del combo Pais (VAL para convertirlo a numero previo al string)
+        'Dim cod As Integer = Val(cbxPaisP.SelectedValue.ToString) 'aca voy a guardar los valores del valueMember del combo Pais (VAL para convertirlo a numero previo al string)
+        With Cmd
+            .CommandType = CommandType.Text
+            '.CommandText = "Select distinct CiudadDistrito From ciudad where PaisCodigo like '" & cod & "'"
+            .CommandText = "Select MODELO,PRECIO,ID From VEHICULOS  where MARCA = '" & cod & "'"
+            .Connection = conn.AbrirConexion
+        End With
+        Da.SelectCommand = Cmd
+        Dt5 = New DataTable 'aca recogere todos los valores
+        Da.Fill(Dt5) 'Relleno dentro de la tabla creada anteriormente
+        With cbxModeloAP 'Recorro y lleno el cbxPaisP con los nombres de la tabla paises
+            .DataSource = Dt5
+            .DisplayMember = "MODELO" 'establece la propiedad que se va a mostrar en el control
+            .ValueMember = "ID"
+        End With
+        drd = Cmd.ExecuteReader
+        If drd.Read() Then
+            txtPrecioAP.Text = drd.Item("PRECIO")
+            'CStr(dgvPacientes.Item(3, dgvPacientes.CurrentRow.Index).Value)
+            '  txtPrecioAP.Text =CStr 'Recorro y lleno el cbxPaisP con los nombres de la tabla paises
+        End If
+    End Sub
+
     Private Sub FormAlta_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Cargo_Combo_Vendedor()
         Cargo_Combo_Cliente()
-        '  Cargo_Combo_TipoPlan()
+        Cargo_Combo_Vehiculos_Marca()
+        Cargo_Combo_TipoPlan()
     End Sub
 
 
@@ -102,5 +110,47 @@ Public Class FormAlta
         '     cerrarConexion()
     End Sub
 
+    ''hacer
+    Public Sub Cargo_Combo_TipoPlan()
 
+        With Cmd
+            .CommandType = CommandType.Text
+            .CommandText = "Select RTRIM(APELLIDO)+' '+RTRIM(NOMBRE) AS APE_NOM,ID From CLIENTE order by APELLIDO asc"
+            .Connection = conn.AbrirConexion 'Abro la conexion
+        End With
+        Da.SelectCommand = Cmd
+        Dt2 = New DataTable 'aca recogere todos los valores
+        Da.Fill(Dt2) 'Relleno dentro de la tabla creada anteriormente
+        With cbxClienteAP  'Recorro y lleno el cbxPaisP con los nombres de la tabla paises
+            .DataSource = Dt2
+            .DisplayMember = "APE_NOM" '& " NOMBRE" 'establece la propiedad que se va a mostrar en el control
+            .ValueMember = "ID" 'establece el valor real de los elementos (seria el id de la tabla para relacionarlo con otra), los valores internos
+
+        End With
+        'Cargo Paises en Tutores
+        '  Cn.Close()
+        '     cerrarConexion()
+    End Sub
+
+    ''hacer2
+
+    Public Sub Cargo_Combo_Vehiculos_Marca()
+
+        With Cmd
+            .CommandType = CommandType.Text
+            .CommandText = "Select distinct MARCA From VEHICULOS order by MARCA asc"
+            .Connection = conn.AbrirConexion 'Abro la conexion
+        End With
+        Da.SelectCommand = Cmd
+        Dt4 = New DataTable 'aca recogere todos los valores
+        Da.Fill(Dt4) 'Relleno dentro de la tabla creada anteriormente
+        With cbxMarcaAP  'Recorro y lleno el cbxPaisP con los nombres de la tabla paises
+            .DataSource = Dt4
+            .DisplayMember = "MARCA" '& " NOMBRE" 'establece la propiedad que se va a mostrar en el control
+            .ValueMember = "MARCA" 'establece el valor real de los elementos (seria el id de la tabla para relacionarlo con otra), los valores internos
+        End With
+
+        ' 
+
+    End Sub
 End Class
